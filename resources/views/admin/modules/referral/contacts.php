8b3cd4ded79d9f285b491750ea59e7f7bd2f183b
rahul1991 <rahulratnam@nethues.com>
2019-08-30 03:35:54 +0530

    worked on referral module blade engen

--- resources/views/admin/modules/referral/contacts.php
+++ resources/views/admin/modules/referral/contacts.php
@@ -1,6 +1,6 @@
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-ui.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/plugins/tables/datatables/datatables.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/pages/datatables_sorting_date.js"></script>
+<script type="text/javascript" src="{{ base_url() }}assets/js/jquery-ui.js"></script>
+<script type="text/javascript" src="{{ base_url() }}assets/js/plugins/tables/datatables/datatables.min.js"></script>
+<script type="text/javascript" src="{{ base_url() }}assets/js/pages/datatables_sorting_date.js"></script>
 <!-- Content area -->
 <div class="content">
 
@@ -12,7 +12,7 @@
                 <div class="panel-heading">
                     <h6 class="panel-title">My Advocates</h6>
                     <div class="heading-elements">
-                        <span class="label bg-success lgraybtn heading-text"><?php echo count($oContacts); ?> Advocates</span>
+                        <span class="label bg-success lgraybtn heading-text">{{ count($oContacts) }} Advocates</span>
                         <button type="button" class="btn btn-link daterange-ranges heading-btn text-semibold">
                             <i class="icon-calendar3 position-left"></i> <span></span> <b class="caret"></b>
                         </button>
@@ -34,11 +34,10 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <?php
+                            @php
                             $inc = 1;
                             foreach ($oContacts as $data) {
-                               
-                                ?>
+                               @endphp
                                 <tr>
                                     <td>			
                                         <div style="vertical-align: top!important;" class="media-left media-middle">
@@ -47,16 +46,16 @@
                                             </a>
                                         </div>
                                         <div class="media-left">
-                                            <a href="javascript:void()" class="text-default text-semibold"><?php echo $data->firstname; ?> <?php echo $data->lastname; ?></a>
-                                            <div class="text-muted text-size-small"><?php echo $data->email; ?></div>
-                                            <div class="text-muted text-size-small"><?php echo $data->mobile == '' ? '<span style="color:#999999">Phone Unavailable</span>' : $data->mobile; ?></div>
+                                            <a href="javascript:void()" class="text-default text-semibold">{{ $data->firstname }} {{ $data->lastname }}</a>
+                                            <div class="text-muted text-size-small">{{ $data->email }}</div>
+                                            <div class="text-muted text-size-small">{!! $data->mobile == '' ? '<span style="color:#999999">Phone Unavailable</span>' : $data->mobile !!}</div>
                                         </div>
                                     </td>
 
-                                    <td><h6 class="text-semibold"><?php echo date('M d, Y', strtotime($data->created)); ?><div class="text-muted text-size-small"><?php echo date('h:i A', strtotime($data->created)) . ' (' . timeAgo($data->created) . ')'; ?></div></h6></td>
+                                    <td><h6 class="text-semibold">{{ date('M d, Y', strtotime($data->created)) }}<div class="text-muted text-size-small">{{ date('h:i A', strtotime($data->created)) . ' (' . timeAgo($data->created) . ')' }}</div></h6></td>
 
-                                    <td><h6 class="text-semibold"><?php echo date('M d, Y', strtotime($data->created)); ?><div class="text-muted text-size-small"><?php echo date('h:i A', strtotime($data->created)) . ' (' . timeAgo($data->created) . ')'; ?></div></h6></td>
-                                    <td><?php echo $data->status == 1 ? '<span class="label bg-success">ACTIVE</span>' : '<span class="label bg-danger">INACTIVE</span>'; ?></td>
+                                    <td><h6 class="text-semibold">{{ date('M d, Y', strtotime($data->created)) }}<div class="text-muted text-size-small">{{ date('h:i A', strtotime($data->created)) . ' (' . timeAgo($data->created) . ')' }}</div></h6></td>
+                                    <td>{!! $data->status == 1 ? '<span class="label bg-success">ACTIVE</span>' : '<span class="label bg-danger">INACTIVE</span>' !!}</td>
                                     <td class="text-center">
                                         <ul class="icons-list">
                                             <li class="dropdown">
@@ -69,22 +68,16 @@
                                             </li>
                                         </ul>
                                     </td>
-                                    <td style="display: none;"><?php echo date('m/d/Y', strtotime($data->created)); ?></td>
-
+                                    <td style="display: none;">{{ date('m/d/Y', strtotime($data->created)) }}</td>
                                 </tr>
-                                <?php $inc++;
-                            } ?>
+                                @php $inc++;
+                            } @endphp
                         </tbody>
                     </table>
                 </div>
-
-
             </div>
-            <!-- <div align="right" id="pagination_link"></div> -->
         </div>
     </div>
-
-
 </div>
 <!-- /content area -->
 
@@ -103,10 +96,6 @@
                 </div>
 
                 <div class="modal-body">
-
-                    <div class="alert-danger" style="margin-bottom:10px;"><?php echo $this->session->userdata('error_message'); ?>
-                        <?php echo validation_errors(); ?></div>
-
                     <div class="form-group">
                         <label class="control-label col-lg-3">First Name</label>
                         <div class="col-lg-9">
@@ -142,8 +131,6 @@
                             <input name="zip" id="zip" class="form-control" value="" type="text" required>
                         </div>
                     </div>
-
-
                 </div>
 
                 <div class="modal-footer">
@@ -169,9 +156,6 @@
                     <h5 class="modal-title"><i class="icon-menu7"></i> &nbsp;Edit Users</h5>
                 </div>
                 <div class="modal-body">
-
-                    <div class="alert-danger" style="margin-bottom:10px;"><?php echo $this->session->userdata('error_message'); ?>
-                        <?php echo validation_errors(); ?></div>
 
                     <div class="form-group">
                         <label class="control-label col-lg-3">First Name</label>
@@ -213,8 +197,6 @@
                             </select>
                         </div>
                     </div>
-
-
                 </div>
                 <div class="modal-footer">
                     <input type="hidden" name="userID" id="e_userID" value="">
@@ -229,76 +211,24 @@
 
 
 <script>
-    /*$(document).ready(function(){
-     
-     var sort_type_element = 'DESC';
-     var sort_by_element = 'id';
-     
-     $(document).on("click", ".sortingAction", function(event){
-     
-     $('.sortingAction').removeClass('sorting');
-     $('.sortingAction').removeClass('sorting_desc');
-     $('.sortingAction').removeClass('sorting_asc');
-     sort_by_element = $(this).attr('sort_by_element');
-     sort_type_element = $(this).attr('sort_type_element');
-     
-     load_user_data(1, sort_by_element, sort_type_element);
-     
-     var newClass = sort_type_element == 'ASC' ? 'sorting_desc' : 'sorting_asc';
-     var newSort = sort_type_element == 'ASC' ? 'DESC' : 'ASC';
-     $('.sortingAction').addClass('sorting');
-     $(this).removeClass('sorting');
-     $(this).addClass(newClass);
-     $(this).attr('sort_type_element', newSort);
-     
-     });
-     
-     function load_user_data(page, sortby, sort_type)
-     {
-     $.ajax({
-     url:"<?php echo base_url(); ?>ajax_pagination/user_pagination/"+page,
-     data: {sortby: sortby, sort_type: sort_type},
-     method:"GET",
-     dataType:"json",
-     success:function(data)
-     {
-     $('#user_table').html(data.user_table);
-     $('#pagination_link').html(data.pagination_link);
-     }
-     });
-     }
-     
-     load_user_data(1, 'id', 'DESC');
-     
-     $(document).on("click", ".pagination li a", function(event){
-     event.preventDefault();
-     var page = $(this).data("ci-pagination-page");
-     load_user_data(page, sort_by_element, sort_type_element);
-     });
-     
-     });*/
-
-
+    
     $(document).ready(function () {
 
         $("#email").on("keyup", function () {
             var sEmail = $("#email").val();
             if (sEmail != '') {
                 $.ajax({
-                    url: "<?php echo base_url('admin/users/checkEmailExist'); ?>",
+                    url: "{{ base_url('admin/users/checkEmailExist') }}",
                     type: "POST",
                     data: {emailID: sEmail},
                     dataType: "json",
                     success: function (data) {
-
                         if (data.status == 'success') {
-
                             $('#emailDiv').addClass('has-error has-feedback');
                             $("#msgEmail").show();
                             $("#msgEmail").html('Email already exist.');
                             $('#addButton').prop("disabled", true);
                         } else {
-
                             $('#emailDiv').removeClass('has-error has-feedback');
                             $("#msgEmail").hide();
                             $("#msgEmail").html('');
@@ -308,6 +238,7 @@
                 });
             }
         });
+		
 
         $(document).on('click', '.userDelete', function () {
 
@@ -318,18 +249,14 @@
                 var userID = $(this).attr('userID');
                 var contactID = $(this).attr('contactID');
                 $.ajax({
-                    url: '<?php echo base_url('admin/users/user_delete'); ?>',
+                    url: "{{ base_url('admin/users/user_delete') }}",
                     type: "POST",
                     data: {userID: userID, contactID: contactID},
                     dataType: "json",
                     success: function (data) {
-
                         if (data.status == 'success') {
-
                             alertMessageAndRedirect('User has been delete successfully.', window.location.href);
-
                         } else {
-
                             alertMessage('Error: Some thing wrong!');
                             $('.overlaynew').hide();
                         }
@@ -338,14 +265,14 @@
             } else {
                 $('.overlaynew').hide();
             }
-
         });
+		
 
         $(document).on('click', '.userEdit', function () {
 
             var userID = $(this).attr('userID');
             $.ajax({
-                url: '<?php echo base_url('admin/users/getUserById'); ?>',
+                url: "{{ base_url('admin/users/getUserById') }}",
                 type: "POST",
                 data: {userID: userID},
                 dataType: "json",
@@ -363,13 +290,11 @@
                         $('#e_infusion_user_id').val(mem.infusion_user_id);
 
                         $("#userLevelEdit").modal();
-
-                    } else {
-
                     }
                 }
             });
         });
+		
 
         $("#updateUsers").submit(function () {
 
@@ -383,7 +308,7 @@
             }
             if (conf == true) {
                 $.ajax({
-                    url: '<?php echo base_url('admin/users/user_update'); ?>',
+                    url: "{{ base_url('admin/users/user_update') }}",
                     type: "POST",
                     data: formData,
                     contentType: false,
@@ -414,7 +339,7 @@
             var formData = new FormData($(this)[0]);
             $('#addButton').prop("disabled", true);
             $.ajax({
-                url: '<?php echo base_url('admin/users/user_add'); ?>',
+                url: "{{ base_url('admin/users/user_add') }}",
                 type: "POST",
                 data: formData,
                 contentType: false,
@@ -422,45 +347,36 @@
                 processData: false,
                 dataType: "json",
                 success: function (data) {
-
                     if (data.status == 'success') {
-
                         alertMessageAndRedirect('User has been add successfully.', window.location.href);
-
                     } else {
-
                         alertMessage('Error: Some thing wrong!');
                         $('.overlaynew').hide();
                     }
                 }
             });
         });
+		
 
         $(document).on('click', '.chg_status', function () {
-
             $('.overlaynew').show();
             var status = $(this).attr('change_status');
             var userId = $(this).attr('userId');
 
             $.ajax({
-                url: '<?php echo base_url('admin/users/update_status'); ?>',
+                url: "{{ base_url('admin/users/update_status') }}",
                 type: "POST",
                 data: {status: status, user_id: userId},
                 dataType: "json",
                 success: function (data) {
-
                     if (data.status == 'success') {
-
                         alertMessageAndRedirect('User has been update successfully.', window.location.href);
-
                     } else {
-
                         alertMessage('Error: Some thing wrong!');
                         $('.overlaynew').hide();
                     }
                 }
             });
         });
-
     });
 </script>		
