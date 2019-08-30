4c724590b51bd66a2d5863a6395b0b6c3ec916f5
rahul1991 <rahulratnam@nethues.com>
2019-08-30 22:40:21 +0530

    worked on referral module blade template

--- resources/views/admin/modules/referral/reports.blade.php
+++ resources/views/admin/modules/referral/reports.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.main_template') 
 
 @section('title')
-<?php echo $title; ?>
+{{ $title }}
 @endsection
 
 @section('contents')
-<?php
+
+@php
 $totalEmailSent = $totalSmsSent = 0;
 if (!empty($oTotalReferralSent)) {
 
@@ -34,7 +35,7 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 	}
 }
 }
-?>
+@endphp
 	<script type="text/javascript" src="http://www.chartjs.org/dist/2.7.2/Chart.bundle.js"></script>
 	
 	<style>
@@ -48,7 +49,7 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 		  <div class="row">
 		  <!--=============Headings & Tabs menu==============-->
 			<div class="col-md-7">
-			  <h3 class="mt20"><img style="width: 18px;" src="<?php echo base_url(); ?>assets/images/refferal_icon.png"> <?php echo $oSettings->title; ?> Report</h3>
+			  <h3 class="mt20"><img style="width: 18px;" src="{{ base_url() }}assets/images/refferal_icon.png"> {{ $oSettings->title }} Report</h3>
 			  
 			</div>
 		  </div>
@@ -67,8 +68,8 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 				<div class="panel panel-flat">
 				  <div class="panel-body p20 pb0 br5 bkg_white text-center ref_stats_sec">
 				  <div class="p20">
-				  	<img width="44" class="br100 mb10" src="<?php echo base_url(); ?>assets/images/ro_icon1.png"/>
-				  	<h1 class="txt_dark fsize28 fw700"><?php echo $totalEmailSent; ?></h1>
+				  	<img width="44" class="br100 mb10" src="{{ base_url() }}assets/images/ro_icon1.png"/>
+				  	<h1 class="txt_dark fsize28 fw700">{{ $totalEmailSent }}</h1>
 				  	<p class="fsize14 txt_dgrey mb0">Referral email sent</p>
 				  </div>
 				  <div class="btop p20">
@@ -81,8 +82,8 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 				<div class="panel panel-flat">
 				  <div class="panel-body p20 pb0 br5 bkg_white text-center ref_stats_sec">
 				  <div class="p20">
-				  	<img width="44" class="br100 mb10" src="<?php echo base_url(); ?>assets/images/ro_icon1.png"/>
-				  	<h1 class="txt_dark fsize28 fw700"><?php echo $totalSmsSent; ?></h1>
+				  	<img width="44" class="br100 mb10" src="{{ base_url() }}assets/images/ro_icon1.png"/>
+				  	<h1 class="txt_dark fsize28 fw700">{{ $totalSmsSent }}</h1>
 				  	<p class="fsize14 txt_dgrey mb0">Referral sms sent</p>
 				  </div>
 				  <div class="btop p20">
@@ -95,8 +96,8 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 				<div class="panel panel-flat">
 				  <div class="panel-body p20 pb0 br5 bkg_white text-center ref_stats_sec">
 				  <div class="p20">
-				  	<img width="44" class="br100 mb10" src="<?php echo base_url(); ?>assets/images/ro_icon2.png"/>
-				  	<h1 class="txt_dark fsize28 fw700"><?php echo $totalOpened; ?></h1>
+				  	<img width="44" class="br100 mb10" src="{{ base_url() }}assets/images/ro_icon2.png"/>
+				  	<h1 class="txt_dark fsize28 fw700">{{ $totalOpened }}</h1>
 				  	<p class="fsize14 txt_dgrey mb0">Opens</p>
 				  </div>
 				  <div class="btop p20">
@@ -109,8 +110,8 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 				<div class="panel panel-flat">
 				  <div class="panel-body p20 pb0 br5 bkg_white text-center ref_stats_sec">
 				  <div class="p20">
-				  	<img width="44" class="br100 mb10" src="<?php echo base_url(); ?>assets/images/ro_icon2.png"/>
-				  	<h1 class="txt_dark fsize28 fw700"><?php echo $totalClicked; ?></h1>
+				  	<img width="44" class="br100 mb10" src="{{ base_url() }}assets/images/ro_icon2.png"/>
+				  	<h1 class="txt_dark fsize28 fw700">{{ $totalClicked }}</h1>
 				  	<p class="fsize14 txt_dgrey mb0">Clicks</p>
 				  </div>
 				  <div class="btop p20">
@@ -128,8 +129,8 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 				<div class="panel panel-flat">
 				  <div class="panel-body p20 pb0 br5 bkg_white text-center ref_stats_sec">
 				  <div class="p20">
-				  	<img width="44" class="br100 mb10" src="<?php echo base_url(); ?>assets/images/ro_icon3.png"/>
-				  	<h1 class="txt_dark fsize28 fw700"><?php echo (count($oRefVisits)) ? count($oRefVisits) : 0; ?></h1>
+				  	<img width="44" class="br100 mb10" src="{{ base_url() }}assets/images/ro_icon3.png"/>
+				  	<h1 class="txt_dark fsize28 fw700">{{ (count($oRefVisits)) ? count($oRefVisits) : 0 }}</h1>
 				  	<p class="fsize14 txt_dgrey mb0">Referral Link(Landing Page) Visits</p>
 				  </div>
 				  <div class="btop p20">
@@ -142,8 +143,8 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 				<div class="panel panel-flat">
 				  <div class="panel-body p20 pb0 br5 bkg_white text-center ref_stats_sec">
 				  <div class="p20">
-				  	<img width="44" class="br100 mb10" src="<?php echo base_url(); ?>assets/images/ro_icon4.png"/>
-				  	<h1 class="txt_dark fsize28 fw700">$<?php echo ($referredAmount) ? $referredAmount : 0; ?></h1>
+				  	<img width="44" class="br100 mb10" src="{{ base_url() }}assets/images/ro_icon4.png"/>
+				  	<h1 class="txt_dark fsize28 fw700">${{($referredAmount) ? $referredAmount : 0}}</h1>
 				  	<p class="fsize14 txt_dgrey mb0">Purchases by referred customer</p>
 				  </div>
 				  <div class="btop p20">
@@ -156,8 +157,8 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 				<div class="panel panel-flat">
 				  <div class="panel-body p20 pb0 br5 bkg_white text-center ref_stats_sec">
 				  <div class="p20">
-				  	<img width="44" class="br100 mb10" src="<?php echo base_url(); ?>assets/images/ro_icon3.png"/>
-				  	<h1 class="txt_dark fsize28 fw700"><?php echo (count($oUntrackedPurchased)) ? count($oUntrackedPurchased) : 0; ?></h1>
+				  	<img width="44" class="br100 mb10" src="{{ base_url() }}assets/images/ro_icon3.png"/>
+				  	<h1 class="txt_dark fsize28 fw700">{{ (count($oUntrackedPurchased)) ? count($oUntrackedPurchased) : 0 }}</h1>
 				  	<p class="fsize14 txt_dgrey mb0">Total Untracked Sales</p>
 				  </div>
 				  <div class="btop p20">
@@ -170,8 +171,8 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 				<div class="panel panel-flat">
 				  <div class="panel-body p20 pb0 br5 bkg_white text-center ref_stats_sec">
 				  <div class="p20">
-				  	<img width="44" class="br100 mb10" src="<?php echo base_url(); ?>assets/images/ro_icon4.png"/>
-				  	<h1 class="txt_dark fsize28 fw700">$<?php echo ($untrackedAmount) ? $untrackedAmount : 0; ?></h1>
+				  	<img width="44" class="br100 mb10" src="{{ base_url() }}assets/images/ro_icon4.png"/>
+				  	<h1 class="txt_dark fsize28 fw700">${{ ($untrackedAmount) ? $untrackedAmount : 0 }}</h1>
 				  	<p class="fsize14 txt_dgrey mb0">Total Untracked Sales Amount</p>
 				  </div>
 				  <div class="btop p20">
@@ -209,51 +210,51 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
                             <tr>
                             	<th style="display: none;"></th>
                             	<th style="display: none;"></th>
-                                <th><i class=""><img src="<?php echo base_url(); ?>assets/images/icon_name.png"></i> Referred Friend</th>
-                                <th><i class=""><img src="<?php echo base_url(); ?>assets/images/icon_name.png"></i> Referrer</th>
-                                <th><i class=""><img src="<?php echo base_url(); ?>assets/images/icon_status.png"></i>Amount</th>
-                                <th><i class=""><img src="<?php echo base_url(); ?>assets/images/icon_status.png"></i>Invoice ID</th>
-                                <th><i class=""><img src="<?php echo base_url(); ?>assets/images/icon_clock.png"></i>Date</th>
+                                <th><i class=""><img src="{{ base_url() }}assets/images/icon_name.png"></i> Referred Friend</th>
+                                <th><i class=""><img src="{{ base_url() }}assets/images/icon_name.png"></i> Referrer</th>
+                                <th><i class=""><img src="{{ base_url() }}assets/images/icon_status.png"></i>Amount</th>
+                                <th><i class=""><img src="{{ base_url() }}assets/images/icon_status.png"></i>Invoice ID</th>
+                                <th><i class=""><img src="{{ base_url() }}assets/images/icon_clock.png"></i>Date</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php
+                            @php
                             $inc = 1;
                             foreach ($oRefPurchased as $data) {
                                 $oTags = \App\Models\Admin\Modules\ReferralModel::getTagsBySaleID($data->id);
 								
-                                ?>
+                                @endphp
                                 <tr>
-                                	<td style="display: none;"><?php echo $data->id; ?></td>
-                                	<td style="display: none;"><?php echo $data->id; ?></td>
+                                	<td style="display: none;">{{ $data->id }}</td>
+                                	<td style="display: none;">{{ $data->id }}</td>
                                     
 
                                     <td>											
-                                        <div class="media-left media-middle"> <?php echo showUserAvtar('', $data->firstname, $data->lastname); ?> </div>
+                                        <div class="media-left media-middle"> {!! showUserAvtar('', $data->firstname, $data->lastname) !!} </div>
                                         <div class="media-left">
-                                            <div class="pt-5"><a href="javascript:void(0);" class="text-default text-semibold bbot"><?php echo $data->firstname; ?> <?php echo $data->lastname; ?></a> <img class="flags" src="<?php echo base_url(); ?>assets/images/flags/<?php echo strtolower($data->country_code); ?>.png" onerror="this.src='<?php echo base_url('assets/images/flags/us.png'); ?>'"/></div>
-                                            <div class="text-muted text-size-small"><?php echo $data->email; ?></div>
+                                            <div class="pt-5"><a href="javascript:void(0);" class="text-default text-semibold bbot">{{ $data->firstname }} {{ $data->lastname }}</a> <img class="flags" src="{{ base_url() }}assets/images/flags/{{ strtolower($data->country_code) }}.png" onerror="this.src='{{ base_url('assets/images/flags/us.png') }}'"/></div>
+                                            <div class="text-muted text-size-small">{{ $data->email }}</div>
                                         </div>
                                     </td>
 
 
                                     <td>											
-                                        <div class="media-left media-middle"> <?php echo showUserAvtar('', $data->aff_firstname, $data->aff_lastname); ?> </div>
+                                        <div class="media-left media-middle"> {!! showUserAvtar('', $data->aff_firstname, $data->aff_lastname) !!} </div>
                                         <div class="media-left">
-                                            <div class="pt-5"><a href="javascript:void(0);" class="text-default text-semibold bbot"><?php echo $data->aff_firstname; ?> <?php echo $data->aff_lastname; ?></a> <img class="flags" src="<?php echo base_url(); ?>assets/images/flags/<?php echo strtolower($data->country_code); ?>.png" onerror="this.src='<?php echo base_url('assets/images/flags/us.png'); ?>'"/></div>
-                                            <div class="text-muted text-size-small"><?php echo $data->aff_email; ?></div>
+                                            <div class="pt-5"><a href="javascript:void(0);" class="text-default text-semibold bbot">{{ $data->aff_firstname }} {{ $data->aff_lastname }}</a> <img class="flags" src="{{ base_url() }}assets/images/flags/{{ strtolower($data->country_code) }}.png" onerror="this.src='{{ base_url('assets/images/flags/us.png') }}'"/></div>
+                                            <div class="text-muted text-size-small">{{ $data->aff_email }}</div>
                                         </div>
                                     </td>
 
-                                    <td><?php echo $data->currency; ?> <?php echo $data->amount; ?></td>
-                                    <td><h6 class="text-semibold"><?php echo $data->invoice_id; ?></h6></td>
-                                    <td><h6 class="text-semibold"><?php echo date('M d, Y', strtotime($data->created)); ?><div class="text-muted text-size-small"><?php echo date('h:i A', strtotime($data->created)); ?></div></h6></td>
+                                    <td>{{ $data->currency }} {{ $data->amount }}</td>
+                                    <td><h6 class="text-semibold">{{ $data->invoice_id }}</h6></td>
+                                    <td><h6 class="text-semibold">{{ date('M d, Y', strtotime($data->created)) }}<div class="text-muted text-size-small">{{ date('h:i A', strtotime($data->created)) }}</div></h6></td>
                                     
                                 </tr>
-                                <?php
+                                @php
                                 $inc++;
                             }
-                            ?>
+                            @endphp
                         </tbody>
                     </table>
                             </div>
@@ -269,70 +270,67 @@ foreach ($oTotalReferralTwillio as $twilliRec) {
 	  </div>
 
 <script type="text/javascript">
-$(document).ready(function () {
-	var tableId = 'allContact';
-	var tableBase = custom_data_table(tableId); 
-});
-</script>
+	$(document).ready(function () {
+		var tableId = 'allContact';
+		var tableBase = custom_data_table(tableId); 
+	});
+
+	window.chartColors2 = {
+		skyblue: 'rgb(35, 131, 152)'
+	};
 	
-	<script>
-
-		window.chartColors2 = {
-	skyblue: 'rgb(35, 131, 152)'
-};
-		
-		var config = {
-			type: 'line',
-			data: {
-				labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
-				datasets: [{
-					label: 'On Site Reviews Report',
-					backgroundColor: window.chartColors2.skyblue,
-					borderColor: window.chartColors2.skyblue,
-					data: [0, 0, 0, 0, 0, 0, 0, 0],
-					fill: false,
-				}]
+	var config = {
+		type: 'line',
+		data: {
+			labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
+			datasets: [{
+				label: 'On Site Reviews Report',
+				backgroundColor: window.chartColors2.skyblue,
+				borderColor: window.chartColors2.skyblue,
+				data: [0, 0, 0, 0, 0, 0, 0, 0],
+				fill: false,
+			}]
+		},
+		options: {
+			responsive: true,
+			title: {
+				display: true,
+				//stext: 'Chart.js Line Chart'
 			},
-			options: {
-				responsive: true,
-				title: {
+			tooltips: {
+				mode: 'index',
+				intersect: false,
+			},
+			hover: {
+				mode: 'nearest',
+				intersect: true
+			},
+			scales: {
+				xAxes: [{
 					display: true,
-					//stext: 'Chart.js Line Chart'
-				},
-				tooltips: {
-					mode: 'index',
-					intersect: false,
-				},
-				hover: {
-					mode: 'nearest',
-					intersect: true
-				},
-				scales: {
-					xAxes: [{
-						display: true,
-						scaleLabel: {
-							display: false,
-							labelString: 'Month'
-						}
-					}],
-					yAxes: [{
+					scaleLabel: {
 						display: false,
-						scaleLabel: {
-							display: false,
-							labelString: 'Value'
-						}
-					}]
-				}
+						labelString: 'Month'
+					}
+				}],
+				yAxes: [{
+					display: false,
+					scaleLabel: {
+						display: false,
+						labelString: 'Value'
+					}
+				}]
 			}
-		};
+		}
+	};
+	
+
+
+	window.onload = function() {
+		var ctx = document.getElementById('canvas3').getContext('2d');
+		window.myLine = new Chart(ctx, config);
+	};
 		
 
-
-		window.onload = function() {
-			var ctx = document.getElementById('canvas3').getContext('2d');
-			window.myLine = new Chart(ctx, config);
-		};
-		
-
-	</script>
+</script>
 @endsection
