4f767f01baf11e3c98c64f7b2191a03b4f04194e
rahul1991 <rahulratnam@nethues.com>
2019-09-17 00:22:35 +0530

    worked on blade template sections

--- resources/views/admin/components/smart-popup/notifications.blade.php
+++ resources/views/admin/components/smart-popup/notifications.blade.php
@@ -1,128 +1,34 @@
-<!--===========TODAY============-->
-    <div class="col-md-12">
-    
-    <?php 
-    /*$inc = 0;
-    if(!empty($notifications_data_today)) { ?>
-    <div style="border-top: none!important;" class="profile_headings">TODAY<a class="pull-right txt_blue2 viewAllNotification" style="text-transform: none; cursor: pointer;"><i class=""><img src="<?php echo base_url(); ?>assets/images/icon_check_blue_double.png" width="16"></i>&nbsp;Mark all read</a></div>
-    <?php  $inc++; 
-	} ?>  
-    
-    
-    <?php
-	 if(!empty($notifications_data_today)) {
-	 	?><div class="p20 pb0"><?php
-		foreach($notifications_data_today as $notification) {
-		$icon = showUserIcon($notification->event_type);
-		?>
-		<div class="media bbot smart_notification">
-			<div class="media-left pr10"><img class="notif_icon" width="36" src="<?php echo base_url(); ?>assets/images/<?php echo $icon; ?>" /></div>
-			<div class="media-left p0"><a href="<?php echo $notification->link; ?>"><p class="txt_dark"><?php echo $notification->message; ?></p></a>
-			<p class="txt_grey"><?php echo $notification->firstname; ?>, <?php echo timeAgoNotification($notification->created); ?></p>
-			</div>
-		</div>
-		<?php
-		}
-		?></div><?php
-	}*/
-	?>
-    
-	</div>
-
-
-<!--===========YESTERDAY============-->
-	<div class="col-md-12">
-
-    <?php /*if(!empty($notifications_data_yesterday)) {?>
-    <div style="border-top: none!important;" class="profile_headings">YESTERDAY <?php if($inc == 0) { ?><a class="pull-right txt_blue2 viewAllNotification" style="text-transform: none; cursor: pointer;"><i class=""><img src="<?php echo base_url(); ?>assets/images/icon_check_blue_double.png" width="16"></i>&nbsp;Mark all read</a><?php $inc++; } ?></div><?php } ?>
-
-    <?php
-	if(!empty($notifications_data_yesterday)) {
-		?>
-        <div class="p20 pb0">
-        	<?php
-        	foreach($notifications_data_yesterday as $notification) {
-
-			$icon = showUserIcon($notification->event_type);
-
-	
-	?>
-	<div class="media bbot smart_notification">
-		<div class="media-left pr10"><img class="notif_icon" width="36" src="<?php echo base_url(); ?>assets/images/<?php echo $icon; ?>" /></div>
-		<div class="media-left p0"><a href="<?php echo $notification->link; ?>"><p class="txt_dark"><?php echo $notification->message; ?></p></a>
-		<p class="txt_grey"><?php echo $notification->firstname; ?>, <?php echo timeAgoNotification($notification->created); ?></p>
-		</div>
-	</div>  
-
-	<?php } ?>
-        </div>
-    <?php }*/ ?>
-	</div>
-
-
 <!--===========LAST WEEK============-->
+<div class="col-md-12">
+    @php 
+		$inc = 0;
+	@endphp
+    
+	@if(!empty($notifications_data))
+		<div style="border-top: none!important;" class="profile_headings">ALL @if($inc == 0)<a class="pull-right txt_blue2 viewAllNotification" style="text-transform: none; cursor: pointer;"><i class=""><img src="{{ base_url() }}assets/images/icon_check_blue_double.png" width="16"></i>&nbsp;Mark all read</a>@php $inc++; @endphp @endif</div>
+	@endif
 
-
-	<div class="col-md-12">
-
-    <?php /*if(!empty($notifications_data_lastweek)) {?>
-    <div style="border-top: none!important;" class="profile_headings">LAST WEEK <?php if($inc == 0) { ?><a class="pull-right txt_blue2 viewAllNotification" style="text-transform: none; cursor: pointer;"><i class=""><img src="<?php echo base_url(); ?>assets/images/icon_check_blue_double.png" width="16"></i>&nbsp;Mark all read</a><?php $inc++; } ?></div><?php } ?>
-
-    <?php
-	if(!empty($notifications_data_lastweek)) {
-		?>
+    @if(!empty($notifications_data))
         <div class="p20 pb0">
-        	<?php
-        	foreach($notifications_data_lastweek as $notification) {
-
-			$icon = showUserIcon($notification->event_type);
-
-	
-	?>
-	<div class="media bbot smart_notification">
-		<div class="media-left pr10"><img class="notif_icon" width="36" src="<?php echo base_url(); ?>assets/images/<?php echo $icon; ?>" /></div>
-		<div class="media-left p0"><a href="<?php echo $notification->link; ?>"><p class="txt_dark"><?php echo $notification->message; ?></p></a>
-		<p class="txt_grey"><?php echo $notification->firstname; ?>, <?php echo timeAgoNotification($notification->created); ?></p>
-		</div>
-	</div>  
-
-	<?php } ?>
-        </div>
-    <?php }*/ ?>
-	</div>
-
-
-<!--===========LAST WEEK============-->
-    <div class="col-md-12">
-
-    <?php 
-    $inc = 0;
-    if(!empty($notifications_data)) {?>
-    <div style="border-top: none!important;" class="profile_headings">ALL <?php if($inc == 0) { ?><a class="pull-right txt_blue2 viewAllNotification" style="text-transform: none; cursor: pointer;"><i class=""><img src="<?php echo base_url(); ?>assets/images/icon_check_blue_double.png" width="16"></i>&nbsp;Mark all read</a><?php $inc++; } ?></div><?php } ?>
-
-    <?php if(!empty($notifications_data)) { ?>
-        <div class="p20 pb0">
-        <?php 
+        @php 
         $inc = 1;
         foreach($notifications_data as $notification) { 
-
         	$icon = showUserIcon($notification->event_type);
-
-        	?>
+        	@endphp
 			<div class="media bbot smart_notification">
-				<div class="media-left pr10"><img class="notif_icon" width="36" src="<?php echo base_url(); ?>assets/images/<?php echo $icon; ?>" /></div>
-				<div class="media-left p0"><a href="<?php echo $notification->link; ?>"><p class="txt_dark"><?php echo $notification->message; ?></p></a>
-				<p class="txt_grey"><?php echo $notification->firstname; ?>, <?php echo timeAgoNotification($notification->created); ?></p>
-				<a class="txt_blue2 fsize12" href="<?php echo $notification->link; ?>">Learn more <i class="icon-arrow-right13 txt_blue2"></i></a>
+				<div class="media-left pr10"><img class="notif_icon" width="36" src="{{ base_url() }}assets/images/{{ $icon }}" /></div>
+				<div class="media-left p0"><a href="{{ $notification->link }}"><p class="txt_dark">{{ $notification->message }}</p></a>
+				<p class="txt_grey">{{ $notification->firstname }}, {{ timeAgoNotification($notification->created) }}</p>
+				<a class="txt_blue2 fsize12" href="{{ $notification->link }}">Learn more <i class="icon-arrow-right13 txt_blue2"></i></a>
 				</div>
 			</div>
-		<?php 
-		if($inc == 6) {
-			break;
-		}
-		$inc++;
-		} ?>
+		@php 
+			if($inc == 6) {
+				break;
+			}
+			$inc++;
+			} 
+		@endphp
         </div>
-    <?php } ?>
-
-	</div>
+	@endif
+</div>
