4f3b6c59b92096e992243970a8fab750f60d9e3a
amit2310 <amitadixtion@gmail.com>
2019-10-09 05:20:59 +0530

    bb 2.0

---
+++ admin_2.0/topbar.php
@@ -0,0 +1,80 @@
+<div class="topbar">
+  <nav class="navbar-custom">
+   <div class="top-bar-top-section mb20">
+    <ul class="list-unstyled topbar-nav float-right mb-0" >
+      <li class="hide-phone app-search">
+        <form role="search" class="">
+          <input type="text" placeholder="Search..." class="form-control">
+        </form>
+      </li>
+      
+      
+      <li class="dropdown"><a class="nav-link dropdown-toggle waves-effect waves-light nav-user top-icons-dropdown" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false"><img src="assets/images/book-next-page.svg" alt="profile-user"> </a>
+        <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i class="dripicons-user text-muted mr-2"></i> Profile</a> <a class="dropdown-item" href="#"><i class="dripicons-wallet text-muted mr-2"></i> My Wallet</a> <a class="dropdown-item" href="#"><i class="dripicons-gear text-muted mr-2"></i> Settings</a> <a class="dropdown-item" href="#"><i class="dripicons-lock text-muted mr-2"></i> Lock screen</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Logout</a></div>
+      </li>
+      
+      
+      <li class="dropdown"><a class="nav-link dropdown-toggle waves-effect waves-light nav-user top-icons-dropdown" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false"><img src="assets/images/messages-bubble-typing-1.svg" alt="profile-user"> </a>
+        <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i class="dripicons-user text-muted mr-2"></i> Profile</a> <a class="dropdown-item" href="#"><i class="dripicons-wallet text-muted mr-2"></i> My Wallet</a> <a class="dropdown-item" href="#"><i class="dripicons-gear text-muted mr-2"></i> Settings</a> <a class="dropdown-item" href="#"><i class="dripicons-lock text-muted mr-2"></i> Lock screen</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Logout</a></div>
+      </li>
+      
+      <li class="dropdown"><a class="nav-link dropdown-toggle waves-effect waves-light nav-user top-icons-dropdown" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false"><img src="assets/images/alarm-bell.svg" alt="profile-user"> </a>
+        <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i class="dripicons-user text-muted mr-2"></i> Profile</a> <a class="dropdown-item" href="#"><i class="dripicons-wallet text-muted mr-2"></i> My Wallet</a> <a class="dropdown-item" href="#"><i class="dripicons-gear text-muted mr-2"></i> Settings</a> <a class="dropdown-item" href="#"><i class="dripicons-lock text-muted mr-2"></i> Lock screen</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Logout</a></div>
+      </li>
+      
+      
+      
+      
+      <li class="dropdown ml-2"><a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false"><img src="assets/images/userprofile01.png" alt="profile-user" class="rounded-circle"> </a>
+        <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i class="dripicons-user text-muted mr-2"></i> Profile</a> <a class="dropdown-item" href="#"><i class="dripicons-wallet text-muted mr-2"></i> My Wallet</a> <a class="dropdown-item" href="#"><i class="dripicons-gear text-muted mr-2"></i> Settings</a> <a class="dropdown-item" href="#"><i class="dripicons-lock text-muted mr-2"></i> Lock screen</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Logout</a></div>
+      </li>
+      
+      
+    </ul>
+    <!--end topbar-nav-->
+    <ul class="list-unstyled topbar-nav top-breadcrumb mb-0">
+      <li>
+        <a href="#"><img src="assets/images/mail-open-line.svg"/> &nbsp;Home</a>
+      </li>
+      
+      <li class="ml10 mr10"><img src="assets/images/chevron-left.svg"/></li>
+      
+      <li>
+        <a href="#"><img src="assets/images/mail-open-line.svg"/> &nbsp;Email</a>
+      </li>
+      
+       <li class="ml10 mr10"><img src="assets/images/chevron-left.svg"/></li>
+       
+      <li>
+        <a href="#">Campaigns</a>
+      </li>
+    </ul>
+    <div class="clearfix"></div>
+    </div>
+    
+    
+    <div class="top-bar-top-section">
+    <ul class="list-unstyled topbar-nav mb-0">
+      <li>
+        <button class="button-menu-mobile nav-link waves-effect waves-light"><img src="assets/images/BACK.svg"/></button>
+      </li>
+      <li class="ml20"><h3 class="htxt_medium_24 dark_700">Dashboard</h3></li>
+    </ul>
+    <div class="clearfix"></div>
+	  </div>
+    
+    
+    
+    
+    
+    
+  </nav>
+  </div>
