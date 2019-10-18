c509a69499c7901e87012003fb3dc3c0317982e4
amit2310 <amitadixtion@gmail.com>
2019-10-19 05:07:20 +0530

    bb 2.0

---
+++ admin_2.0/sample.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Brand Boost 2.0</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="icon" href="assets/images/icon.ico" sizes="16x16" type="image/ico">
+
+
+ <!--******************
+ CSS
+ **********************-->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/metisMenu.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+<link href="assets/css/styleguide.css" rel="stylesheet" type="text/css">
+<!--<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">-->
+
+
+</head>
+<body id="">
+
+<header class="p20 bbot">
+<div class="container">
+	<div class="row">
+  		<div class="col">
+  			<div class="bkg_blue_200 p25 br4">Box1</div>
+  		</div>
+  		<div class="col">
+  			<div class="bkg_blue_200 p25 br4">Box1</div>
+  		</div>
+  		<div class="col">
+  			<div class="bkg_blue_200 p25 br4">Box1</div>
+  		</div>
+  		
+  		
+  		<div class="col">
+  			<div class="bkg_blue_200 p25 br4">Box1</div>
+  		</div>
+  		<div class="col">
+  			<div class="bkg_blue_200 p25 br4">Box1</div>
+  		</div>
+  		
+  		
+  		
+  		
+  		
+  	</div>
+</div>
+</header>
+  
+
+  
+  
+  
+  
+  
+  
+
+ 
+ 
+ 
+ 
+ 
+ 
+ <!--******************
+  jQuery
+ **********************-->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/metisMenu.min.js"></script>
+<script src="assets/js/waves.min.js"></script>
+<script src="assets/js/jquery.slimscroll.min.js"></script>
+<script src="assets/js/app.js"></script>
+
+</body>
+</html>
