692f33f49e660317c00ab34af8547853b3228157
andrew-bbio <andrew@brandboost.io>
2019-10-18 16:37:20 -0400

    Vue JS Updates

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -12,7 +12,8 @@ import Media from './components/admin/brandboost/Media.vue';
 import Tags from './components/admin/tags/';
 import TagsReview from './components/admin/tags/tagsreview';
 import TagsFeedback from './components/admin/tags/tagsfeedback';
-
+import EmailLists from './components/admin/Lists';
+import EmailBroadcasts from './components/admin/broadcast/Email';
 
 const routes = [
     { path: '/', component: Dashboard },
@@ -31,7 +32,9 @@ const routes = [
     { path: '/brandboost/media', component: Media, props: {title : 'On Site Brand Boost Media'} },
     { path: '/tags', component: Tags, meta: { title: 'Insight Tags - Brand Boost'} },
     { path: '/tags/tagsreview', component: TagsReview, meta: { title: 'Tags Review - Brand Boost'} },
-    { path: '/tags/tagsfeedback', component: TagsFeedback, meta: { title: 'Tags Feedback - Brand Boost'} }
+    { path: '/tags/tagsfeedback', component: TagsFeedback, meta: { title: 'Tags Feedback - Brand Boost'} },
+    { path: '/lists', component: EmailLists, meta: { title: 'Email Lists - Brand Boost'} },
+    { path: '/broadcast/email', component: EmailBroadcasts, meta: { title: 'Email Broadcast - Brand Boost'} }
 ];
 
 export default routes;
