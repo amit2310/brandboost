2929182cc08769e1e922c402c237d961a62aeb2b
amit2310 <amitadixtion@gmail.com>
2019-11-02 04:28:38 +0530

    bb 2.0

--- admin_2.0/email_workflow2.php
+++ admin_2.0/email_workflow2.php
@@ -14,7 +14,9 @@
 <link href="assets/css/style.css" rel="stylesheet" type="text/css">
 <link href="assets/css/styleguide.css" rel="stylesheet" type="text/css">
 
-
+<style>
+.apexcharts-canvas{margin: 0 auto;}
+</style>
 
 </head>
 <body id="EmailSection">
@@ -41,11 +43,11 @@
    <div class="row">
    	<div class="col-md-6">
    	<span class="float-left mr20"><img src="assets/images/BACK.svg"/></span>
-   	<h3 class="htxt_medium_24 dark_700">Email Campaigns</h3>
+   	<h3 class="htxt_medium_24 dark_700">Email Workflows</h3>
    	</div>
    	<div class="col-md-6 text-right">
    		<button class="circle-icon-40 mr15"><img src="assets/images/emailfilter.svg"/></button>
-   		<button class="btn btn-md bkg_email_300 light_000 slidebox"> New campaign <span style="opacity: 0.3"><img src="assets/images/blue-plus.svg"/></span></button>
+   		<button class="btn btn-md bkg_email_300 light_000 slidebox"> Create new <span style="opacity: 0.3"><img src="assets/images/blue-plus.svg"/></span></button>
    	</div>
    </div>
    </div>
@@ -97,8 +99,8 @@
     
     
     <div class="row">
-    	<div class="col-md-3">
-    		<div class="card p0 pt30 min_h_275 text-center animate_top">
+    	<div class="col-md-3 d-flex">
+    		<div class="card p0 pt30 min_h_275 text-center animate_top col">
     			<div class="dot_dropdown">
 					<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
 				   <img class="" src="assets/images/dots.svg" alt="profile-user"> </a>
@@ -107,10 +109,11 @@
 				  <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Logout</a></div>
 				</div>
 				
-				<a href="#" class="circle-icon-64 bkg_email_000 m0auto"><img src="assets/images/send-plane-fill-email_blue.svg"/> </a>
-   				<h3 class="htxt_bold_16 dark_700 mb-1 mt-4">2nd Training Email</h3>
-   				<p class="fsize11 fw500 dark_200 text-uppercase">Campaign</p>
+				<a href="#" class="circle-icon-64 bkg_email_000 m0auto"><img src="assets/images/flashlight-fill.svg"/> </a>
+   				<h3 class="htxt_bold_16 dark_700 mb-1 mt-4">User Onboarding</h3>
+   				<p class="fsize11 fw500 dark_200 text-uppercase">WORKFLOW</p>
    				<div style="min-height: 40px; margin: 4px 0 10px;" class="img_box">
+   					
    					<div id="sparklinechart1"></div>
    				</div>
    				
@@ -125,8 +128,11 @@
     		</div>
     	</div>
     	
-    	<div class="col-md-3">
-    		<div class="card p0 pt30 min_h_275 text-center animate_top">
+    	
+    	
+    	
+    	<div class="col-md-3 d-flex">
+    		<div class="card p0 pt30 min_h_275 text-center animate_top col">
     			<div class="dot_dropdown">
 					<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
 				   <img class="" src="assets/images/dots.svg" alt="profile-user"> </a>
@@ -135,37 +141,9 @@
 				  <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Logout</a></div>
 				</div>
 				
-				<a href="#" class="circle-icon-64 bkg_dark_000 m0auto"><img src="assets/images/send-plane-fill-grey.svg"/> </a>
-   				<h3 class="htxt_bold_16 dark_700 mb-1 mt-4">2nd Training Email</h3>
-   				<p class="fsize11 fw500 dark_200 text-uppercase">Campaign</p>
-   				<div style="min-height: 40px; margin: 4px 0;" class="img_box">
-   					<img src="assets/images/Rectangle-grey.svg"/>	
-   				</div>
-   				
-   			<div class="p15 pt15 btop">
-							<ul class="workflow_list">
-								<li><a href="#">DRAFT</a></li>
-								
-							</ul>
-						</div>
-   		
-    		</div>
-    	</div>
-    	
-    	
-    	<div class="col-md-3">
-    		<div class="card p0 pt30 min_h_275 text-center animate_top">
-    			<div class="dot_dropdown">
-					<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-				   <img class="" src="assets/images/dots.svg" alt="profile-user"> </a>
-					<div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i class="dripicons-user text-muted mr-2"></i> Profile</a> <a class="dropdown-item" href="#"><i class="dripicons-wallet text-muted mr-2"></i> My Wallet</a> <a class="dropdown-item" href="#"><i class="dripicons-gear text-muted mr-2"></i> Settings</a> <a class="dropdown-item" href="#"><i class="dripicons-lock text-muted mr-2"></i> Lock screen</a>
-				  <div class="dropdown-divider"></div>
-				  <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Logout</a></div>
-				</div>
-				
-				<a href="#" class="circle-icon-64 bkg_email_000 m0auto"><img src="assets/images/send-plane-fill-email_blue.svg"/> </a>
-   				<h3 class="htxt_bold_16 dark_700 mb-1 mt-4">2nd Training Email</h3>
-   				<p class="fsize11 fw500 dark_200 text-uppercase">Campaign</p>
+				<a href="#" class="circle-icon-64 bkg_email_000 m0auto"><img src="assets/images/flashlight-fill.svg"/> </a>
+   				<h3 class="htxt_bold_16 dark_700 mb-1 mt-4">User Onboarding</h3>
+   				<p class="fsize11 fw500 dark_200 text-uppercase">WORKFLOW</p>
    				<div style="min-height: 40px; margin: 4px 0 10px;" class="img_box">
    					<div id="sparklinechart2"></div>
    				</div>
@@ -182,10 +160,10 @@
     	</div>
     	
     	
-    	<div class="col-md-3 text-center">
-    		<div class="card p30 min_h_275 animate_top">
+    	<div class="col-md-3 text-center d-flex">
+    		<div class="card p30 min_h_275 animate_top col">
      		<img class="mt20 mb30" src="assets/images/plus_icon_circle_64.svg">
-     		<p class="htxt_regular_16 dark_100 mb15">Create<br>contacts list</p>
+     		<p class="htxt_regular_16 dark_100 mb15">Create<br>new segment</p>
      		</div>
     	</div>
     </div>
@@ -212,7 +190,7 @@
  <!--******************
   Create Contact Sliding Smart Popup
  **********************-->
- <?php include("email_campaign_create_smart_popup.php"); ?>
+ <?php include("email_workflow_create_smart_popup.php"); ?>
  
     
  
@@ -246,7 +224,6 @@ $(document).ready(function(){
 	$(".main-icon-menu-pane.email").addClass("active");
 	$(".main-icon-menu-pane.people").removeClass("active");
 </script>
-
 
 
   <script src="https://cdn.jsdelivr.net/npm/apexcharts@latest"></script>
