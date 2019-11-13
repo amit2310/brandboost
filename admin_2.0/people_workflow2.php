3a8bbda47aba241da70d73c401d4ebc34aa49eaf
amit2310 <amitadixtion@gmail.com>
2019-11-14 04:52:50 +0530

    bb 2.0

--- admin_2.0/people_workflow2.php
+++ admin_2.0/people_workflow2.php
@@ -15,9 +15,7 @@
 <link href="assets/css/styleguide.css" rel="stylesheet" type="text/css">
 <link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
 	<style>
-		.navbar-custom{background: #ffffff!important; padding-left: 320px}
-		.top-bar-top-section{padding-left: 310px}
-		.content-area{padding-left: 320px;}
+		.navbar-custom{background: #ffffff!important}
 	</style>
 </head>
 
@@ -39,6 +37,7 @@
 
   
   
+
   <div class="h-100 workflow_dots_bkg">
  <!--******************
   TOPBAR
@@ -178,143 +177,7 @@
     </div>
   </div>
   
-  
-  <!--******************
- PAGE SIDEBAR
- **********************-->
-	<div class="page_sidebar bkg_light_000 absl">
-	 	<div class="inner2 pb0">
-			<div class="title-box">
-			  <h6 class="menu-title" style="line-height: 36px;"><span class="button-menu-mobile_sidebar"><img src="assets/images/close_menu_circle.svg"></span> &nbsp; Open Menu</h6>
-			</div>
-			<h3 class="htxt_medium_20 dark_800" >Nodes </h3>
-			<hr />
-			
-			<div class="sidebar_search_big">
-        	<input type="text" name="" value="" placeholder="Search">
-        	<button class="sidebar_search_submit"><img src="assets/images/submit_grey.svg"></button>
-        </div>
-        </div> 
-        
-        <div class="sidebar_headings">
-        	<span class="float-left">TRIGGERS</span>
-        	<i class="float-right"><img src="assets/images/arrow-down-s-line.svg"/></i>
-        	<div class="clearfix"></div>
-        </div>
-        
-        
-        <div class="p20 pt0 pb0">
-        	<ul class="list_with_icons">
-        	<li>
-        	<div class="media_left">
-        		<span class="circle_32 img bkg_blue_400"><img src="assets/images/user-3-fill.svg"/></span>
-        	</div>
-        	<div class="media_left">
-        		<p class="htxt_bold_14 dark_600">Contact</p>
-        		<p class="dark_300 fw300 fsize12">Eu maecenas nisl interdum.</p>
-        	</div>
-        	</li>
-        	
-        	
-        	<li>
-        	<div class="media_left">
-        		<span class="circle_32 img bkg_reviews_300"><img src="assets/images/file-list-2-fill.svg"/></span>
-        	</div>
-        	<div class="media_left">
-        		<p class="htxt_bold_14 dark_600">Form</p>
-        		<p class="dark_300 fw300 fsize12">Tellus lectus platea.</p>
-        	</div>
-        	</li>
-        	
-        	<li>
-        	<div class="media_left">
-        		<span class="circle_32 img bkg_dark_300"><img src="assets/images/time-fill.svg"/></span>
-        	</div>
-        	<div class="media_left">
-        		<p class="htxt_bold_14 dark_600">Timer</p>
-        		<p class="dark_300 fw300 fsize12">Arcu blandit duis phasellus.</p>
-        	</div>
-        	</li>
-        	
-        	
-        	
-        	
-        </ul>
-        <div class="clearfix"></div>
-        </div>
-        
-        
-        
-        <div class="sidebar_headings">
-        	<span class="float-left">ACTIONS</span>
-        	<i class="float-right"><img src="assets/images/arrow-down-s-line.svg"/></i>
-        	<div class="clearfix"></div>
-        </div>
-        
-        
-        
-        
-        
-        <div class="p20 pt0 pb0">
-        	<ul class="list_with_icons">
-        	<li>
-        	<div class="media_left">
-        		<span class="circle_32 img bkg_brand_300"><img src="assets/images/send-plane-fill.svg"/></span>
-        	</div>
-        	<div class="media_left">
-        		<p class="htxt_bold_14 dark_600">Email</p>
-        		<p class="dark_300 fw300 fsize12">Semper aliquet viverra libero.</p>
-        	</div>
-        	</li>
-        	
-        	
-        	<li>
-        	<div class="media_left">
-        		<span class="circle_32 img bkg_sms_400"><img src="assets/images/message-2-fill.svg"/></span>
-        	</div>
-        	<div class="media_left">
-        		<p class="htxt_bold_14 dark_600">SMS</p>
-        		<p class="dark_300 fw300 fsize12">Amet nec euismod.</p>
-        	</div>
-        	</li>
-        	
-        	<li>
-        	<div class="media_left">
-        		<span class="circle_32 img bkg_reviews_400"><img src="assets/images/star-s-fill.svg"/></span>
-        	</div>
-        	<div class="media_left">
-        		<p class="htxt_bold_14 dark_600">Review</p>
-        		<p class="dark_300 fw300 fsize12">Cras aenean vitae duis amet.</p>
-        	</div>
-        	</li>
-        	
-        	<li>
-        	<div class="media_left">
-        		<span class="circle_32 img bkg_red_400"><img src="assets/images/notification-badge-fill.svg"/></span>
-        	</div>
-        	<div class="media_left">
-        		<p class="htxt_bold_14 dark_600">Notification</p>
-        		<p class="dark_300 fw300 fsize12">Mi vitae tortor, eget. </p>
-        	</div>
-        	</li>
-        	
-        	
-        	
-        	
-        </ul>
-        <div class="clearfix"></div>
-        </div>
-        
-        
-        
 
-    
-    <div class="clearfix"></div>   
-</div>
-  
-<!--******************
- END PAGE SIDEBAR
- **********************-->
   
   
    </div>
@@ -337,6 +200,15 @@
  
  
 
+ 
+ <!--******************
+  Create Contact Sliding Smart Popup
+ **********************-->
+ <?php include("workflow_nodes_smart_popup.php"); ?>
+ 
+    
+ 
+ 
  
  
  <!--******************
@@ -368,6 +240,17 @@ $("#fullscreen").click(function(){
 	
 $(".touchspin-step").TouchSpin({
 	step: 10
+});
+</script>
+
+
+<script>
+$(document).ready(function(){
+	$(".slidebox").click(function(){
+		$(".box").animate({
+			width: "toggle"
+		});
+	});
 });
 </script>
 
