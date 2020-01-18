07316413f573b0e42545f900451933af3f014414
umair-clickdrop <umair@nethues.com>
2020-01-18 07:26:09 +0530

    Web Chat updates

---
+++ resources/js/components/admin/chat/web/partials/SaveReplyPopup.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="box" style="width: 424px;">
+        <div style="width: 424px;overflow: hidden; height: 100%;">
+            <div style="height: 100%; overflow-y:auto; overflow-x: hidden;"> <a class="cross_icon slidebox"><i class=""><img src="assets/images/cross.svg"/></i></a>
+                <div class="p40">
+                    <div class="row">
+                        <div class="col-md-12"> <img src="assets/images/saved_reply_forum.svg"/>
+                            <h3 class="htxt_medium_24 dark_800 mt20">Create Saved Reply </h3>
+                            <hr>
+                        </div>
+                        <div class="col-md-12">
+                            <form action="/action_page.php">
+                                <div class="form-group">
+                                    <label class="fsize11" for="fname">REPLY NAME</label>
+                                    <input type="text" class="form-control h56 fsize13" id="fname" placeholder="Name (Internal only)" name="fname">
+                                </div>
+                                <div class="form-group">
+                                    <label class="fsize11" for="desc">REPLY CONTENT</label>
+                                    <textarea class="form-control min_h_185 p20 pt10 fsize13" id="desc" placeholder="Reply content goes here..."></textarea>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="row bottom-position">
+                        <div class="col-md-12 mb15">
+                            <hr>
+                        </div>
+                        <div class="col-md-12">
+                            <button class="btn btn-lg bkg_blue_300 light_000 pr20 min_w_160 fsize16 fw600">Save Reply</button>
+                            <a class="blue_300 fsize16 fw600 ml20" href="#">Close</a> </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {};
+</script>
---
+++ resources/js/components/admin/chat/web/partials/WebProfileBar.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="box d-block" style="width: 280px; top: 129px!important; height: calc(100% - 129px); max-height: 798px; position: absolute!important; box-shadow: none!important; border-left: 1px solid #f5f6f8">
+        <div style="width: 280px;overflow: hidden; height: 100%;">
+            <div style="height: 100%; overflow-y:auto; overflow-x: hidden;">
+                <div class="p25 pl20 pr20">
+                    <div class="pt0 pb20 bbot">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h3 class="fsize18 dark_800 fw500">Profile</h3>
+                            </div>
+                            <div class="col-md-6">
+                                <!--<a class="slidebox float-right"><i class=""><img width="24" src="assets/images/cross@2x.png"/></i></a>-->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt30 pb30 bbot">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <div class="profile_image_bkg mb20">
+                                    <img width="72" src="assets/images/avatar/02.png"/>
+                                </div>
+                                <div class="bkg_green_light green_400 fsize10 pl10 pr10 pt-1 pb-0 br8 d-inline-block">Lead</div>
+                                <h3 class="fsize16 fw500 dark_700 mb0 mt10">Norma Alexander</h3>
+                                <p class="fsize14 fw400 dark_300 mb20">terra.hamilton@example.com</p>
+                                <ul class="profile_social_icon">
+                                    <li><a href="#"><img src="assets/images/Facebook.svg"/></a></li>
+                                    <li><a href="#"><img src="assets/images/twitter.svg"/></a></li>
+                                    <li><a href="#"><img src="assets/images/linkedin.svg"/></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt20 pb20 bbot">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3 class="fsize16 fw500 dark_700 mb-2 mt0">Details</h3>
+                                <ul class="user_details_list">
+                                    <li>(239) 555-0108</li>
+                                    <li>435036</li>
+                                    <li>3 days ago</li>
+                                    <li>1 year ago</li>
+                                    <li>Paris, France</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt20 pb20">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3 class="fsize16 fw500 dark_700 mt0 mb-2">Tags</h3>
+                                <button class="tag_btn br8">Chat</button>
+                                <button class="tag_btn br8">Website visitor</button>
+                                <button class="btn-link" style="border: none; background: none;"><img src="assets/images/plus_grey.svg"/></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {};
+</script>
