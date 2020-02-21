4e7b350bb027ef4d88eec76926101e734b986c1f
amit2310 <amitadixtion@gmail.com>
2020-02-22 03:46:30 +0530

    bb 2.0

---
+++ admin_2.0/review_feed_new2.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Brand Boost 2.0</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="icon" href="assets/images/icon.ico" sizes="16x16" type="image/ico">
+
+
+ <!--******************
+ CSS
+ **********************-->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+<link href="assets/css/styleguide.css" rel="stylesheet" type="text/css">
+
+
+
+
+</head>
+<body id="ReviewSection">
+
+<div class="page-wrapper">
+ <!--******************
+ SIDEBAR
+ **********************-->
+  <?php include("sidebar.php"); ?>
+ 
+
+  <div class="page-content">
+ <!--******************
+  TOPBAR
+ **********************-->
+  <?php include("topbar.php"); ?>
+  
+  
+ <!--******************
+  Top Heading area
+ **********************-->
+  <div class="top-bar-top-section bbot">
+  <div class="container-fluid">
+   <div class="row">
+   	<div class="col-md-6">
+   	<span class="float-left mr20"><img src="assets/images/BACK.svg"/></span>
+   	<h3 class="htxt_medium_24 dark_700">Reviews Feed</h3>
+   	</div>
+   	<div class="col-md-6 col-6 text-right">
+   		<button class="circle-icon-40 mr15"><img width="16" src="assets/images/settings-2-line.svg"></button>
+   		<!--<button class="btn btn-md bkg_reviews_400 light_000 slidebox">ADD New Contact <span><img src="assets/images/reviews_plus_icon.svg"></span></button>-->
+   	</div>
+   </div>
+   </div>
+    <div class="clearfix"></div>
+</div>
+	 
+	  
+	  
+	  
+ <!--******************
+  Content Area
+ **********************-->
+   <div class="content-area">
+    <div class="container-fluid">
+    
+    <div class="table_head_action">
+    <div class="row">
+    	<div class="col-md-6">
+    		<ul class="table_filter">
+    			<li><a class="active" href="#">ALL</a></li>
+    			<li><a href="#">POSTED</a></li>
+    			<li><a href="#">DRAFT</a></li>
+    			<li><a href="#">POSITIVE</a></li>
+    			<li><a href="#">NEGATIVE</a></li>
+    			<li><a href="#"><i><img src="assets/images/filter-3-fill.svg"></i> &nbsp; FILTER</a></li>
+    		</ul>
+    	</div>
+    	<div class="col-md-6">
+    		<ul class="table_filter text-right">
+    			<li><a href="#"><i><img src="assets/images/search-2-line_grey.svg"></i></a></li>
+    			<li><a href="#"><i><img src="assets/images/sort_16_grey.svg"></i></a></li>
+    			<li><a href="#"><i><img src="assets/images/cards_16_grey.svg"></i></a></li>
+    		</ul>
+    	</div>
+    </div>
+    </div>
+    
+    
+    
+    
+    <div class="row">
+    	<div class="col-md-12">
+    		<div class="card p0">
+    			<span style="right: 25px; top: 23px; left: auto" class="status_icon bkg_green_300"></span>
+						<div class="p25">
+							<div class="mb-2">
+								<div class="row">
+									<div class="col-md-8">
+										<p class="fsize14 fw400 dark_600 float-left mr-3 lh_26"><span class="circle_icon_24 bkg_reviews_400 mr-2"><img src="assets/images/pie_chart_fill_12.svg"></span> &nbsp; Gladys Russell</p>
+										<p class="mt-0 review_rating_start float-left"><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_grey_18.svg"></i></p>
+
+										<p class="float-left ml-3 dark_400">4.0</p>
+									</div>
+									
+									<div class="col-md-4 text-right">
+						  			<p class="float-right fsize14 dark_400 mt-0 mb-0 mr-5">Mar 17, 2014 1:06 AM </p>
+									</div>
+								</div>
+							</div>
+							
+							
+							<p class="fsize14 fw400 dark_600 lh_24">
+								The Top Forex Brokers Review is a great website that provided me with insightful information about Forex Trading by incorporating a list of the top brokers in the world, it gave me an idea of where i should make trades. The use of Forex Education is fantastic as it helped me gain a better understanding of various aspects related to the Forex Trading, which allowed me to make my decision of which broker to trade with. 
+								I highly recommend using this website it gave me an idea of where i should make trades.
+							</p>
+							
+							<div class="reply_sec_link">
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/reply_grey_16.svg"/> &nbsp; Reply</a>
+							<div class="clearfix"></div>	
+							</div>
+							
+						</div>
+					</div>
+					
+					
+					<div class="card p0">
+    			<span style="right: 25px; top: 23px; left: auto" class="status_icon bkg_reviews_300"></span>
+						<div class="p25">
+							<div class="mb-2">
+								<div class="row">
+									<div class="col-md-8">
+										<p class="fsize14 fw400 dark_600 float-left mr-3 lh_26"><span class="circle_icon_24 bkg_blue_200 mr-2"><img src="assets/images/google_14_white.svg"></span> &nbsp; Mitchell Russell</p>
+										<p class="mt-0 review_rating_start float-left"><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_grey_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_grey_18.svg"></i></p>
+
+										<p class="float-left ml-3 dark_400">3.0</p>
+									</div>
+									
+									<div class="col-md-4 text-right">
+						  			<p class="float-right fsize14 dark_400 mt-0 mb-0 mr-5">Mar 17, 2014 1:06 AM </p>
+									</div>
+								</div>
+							</div>
+							
+							
+							<p class="fsize14 fw400 dark_600 lh_24">
+								Ea commodo occaecat sunt aute nostrud dolore duis consectetur esse. Occaecat minim nisi duis magna labore occaecat in minim tempor voluptate ut laboris do dolore. Pariatur in deserunt aute officia dolore veniam velit. Deserunt qui ullamco aute exercitation ad exercitation consectetur excepteur dolore excepteur eiusmod nostrud.
+							</p>
+							<div class="reply_sec_link">
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/comment_grey_16.svg"/> &nbsp; 3 Comments</a>	
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/thumb-up-grey-16.svg"/> &nbsp; 13 Likes</a>	
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/reply_grey_16.svg"/> &nbsp; Reply</a>
+							<div class="clearfix"></div>	
+							</div>
+							
+						</div>
+					</div>
+					
+					<div class="card p0">
+    			<span style="right: 25px; top: 23px; left: auto" class="status_icon bkg_light_600"></span>
+						<div class="p25">
+							<div class="mb-2">
+								<div class="row">
+									<div class="col-md-8">
+										<p class="fsize14 fw400 dark_600 float-left mr-3 lh_26"><span class="circle_icon_24 bkg_blue_400 mr-2"><img src="assets/images/facebook_white_14.svg"></span> &nbsp; Floyd Black</p>
+										<p class="mt-0 review_rating_start float-left"><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i></p>
+
+										<p class="float-left ml-3 dark_400">5.0</p>
+									</div>
+									
+									<div class="col-md-4 text-right">
+						  			<p class="float-right fsize14 dark_400 mt-0 mb-0 mr-5">Mar 17, 2014 1:06 AM </p>
+									</div>
+								</div>
+							</div>
+							
+							
+							<p class="fsize14 fw400 dark_600 lh_24">
+								The Top Forex Brokers Review is a great website that provided me with insightful information about Forex Trading by incorporating a list of the top brokers in the world, it gave me an idea of where i should make trades. The use of Forex Education is fantastic as it helped me gain a better understanding of various aspects related to the Forex Trading, which allowed me to make my decision of which broker to trade with. 
+I highly recommend using this website it gave me an idea of where i should make trades.
+							</p>
+							<div class="reply_sec_link">
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/comment_grey_16.svg"/> &nbsp; 3 Comments</a>	
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/thumb-up-grey-16.svg"/> &nbsp; 13 Likes</a>	
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/reply_grey_16.svg"/> &nbsp; Reply</a>
+							<div class="clearfix"></div>	
+							</div>
+							
+						</div>
+					</div>
+					
+					
+					<div class="card p0">
+    			<span style="right: 25px; top: 23px; left: auto" class="status_icon bkg_reviews_300"></span>
+						<div class="p25">
+							<div class="mb-2">
+								<div class="row">
+									<div class="col-md-8">
+										<p class="fsize14 fw400 dark_600 float-left mr-3 lh_26"><span class="circle_icon_24 bkg_blue_200 mr-2"><img src="assets/images/window_w_14.svg"></span> &nbsp; Arlene Lane</p>
+										<p class="mt-0 review_rating_start float-left"><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_yellow_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_grey_18.svg"></i><i class=""><img width="14" src="assets/images/star-fill_grey_18.svg"></i></p>
+
+										<p class="float-left ml-3 dark_400">3.0</p>
+									</div>
+									
+									<div class="col-md-4 text-right">
+						  			<p class="float-right fsize14 dark_400 mt-0 mb-0 mr-5">Mar 17, 2014 1:06 AM </p>
+									</div>
+								</div>
+							</div>
+							
+							
+							<p class="fsize14 fw400 dark_600 lh_24">
+								Eiusmod deserunt mollit cillum nostrud aute eiusmod. Lorem minim exercitation Lorem cillum non qui adipisicing amet laborum cupidatat commodo dolore sunt. Proident voluptate exercitation voluptate id. Eiusmod deserunt mollit cillum nostrud aute eiusmod. Lorem minim exercitation Lorem cillum non qui adipisicing amet laborum cupidatat commodo dolore sunt. Proident voluptate exercitation voluptate id. Eiusmod deserunt mollit cillum nostrud aute eiusmod. Lorem minim exercitation Lorem cillum non qui adipisicing amet laborum cupidatat commodo dolore sunt. Proident voluptate exercitation voluptate id.
+							</p>
+							<div class="reply_sec_link">
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/comment_grey_16.svg"/> &nbsp; 3 Comments</a>	
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/thumb-up-grey-16.svg"/> &nbsp; 13 Likes</a>	
+							<a class="text-uppercase dark_200 fsize11 fw500 ls_4" href="#"><img src="assets/images/reply_grey_16.svg"/> &nbsp; Reply</a>
+							<div class="clearfix"></div>	
+							</div>
+							
+						</div>
+					</div>
+					
+					
+					
+    	</div>
+    </div>
+     
+      </div>
+      
+      </div>
+      
+<!--******************
+  Content Area End
+ **********************-->
+  </div>
+  </div>
+  
+  
+ 
+ 
+ <!--******************
+  Create Contact Sliding Smart Popup
+ **********************-->
+ <?php include("email_campaign_create_smart_popup.php"); ?>
+ 
+ 
+ 
+ <!--******************
+  jQuery
+ **********************-->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/metisMenu.min.js"></script>
+<script src="assets/js/waves.min.js"></script>
+<script src="assets/js/jquery.slimscroll.min.js"></script>
+<script src="assets/js/app.js"></script>
+
+<script>
+$(document).ready(function(){
+	$(".slidebox").click(function(){
+		$(".box").animate({
+			width: "toggle"
+		});
+	});
+});
+</script>
+
+
+<script>
+	//side nav active script
+	$(".nav-link.review").addClass("active");
+	$(".nav-link.people").removeClass("active");
+	$(".main-icon-menu-pane.review").addClass("active");
+	$(".main-icon-menu-pane.people").removeClass("active");
+</script>
+
+
+</body>
+</html>
