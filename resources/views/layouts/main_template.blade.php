d29a8f07cf42c142729acefd568fa7fc09a5e0f5
umair-clickdrop <umair@nethues.com>
2020-03-06 08:19:21 +0530

    Minor change

--- resources/views/layouts/main_template.blade.php
+++ resources/views/layouts/main_template.blade.php
@@ -108,6 +108,12 @@
     CSS
     **********************-->
     @include('layouts.main_partials._main_css')
+    <style>
+        svg path,
+        svg rect{
+            fill: #FF6700;
+        }
+    </style>
 
 </head>
 <body id="PeopleSection">
@@ -157,7 +163,32 @@
         @include('layouts.main_partials._main_topmenu')
 
         <div id="masterContainer2">
-            <router-view></router-view>
+            <router-view>
+                <div style="width:100%;text-align:center;margin-top:30px;">
+                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                         x="0px" y="0px"
+                         width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;display:block;margin:0 auto;" xml:space="preserve">
+    <rect x="0" y="0" width="4" height="10" fill="#333">
+        <animateTransform attributeType="xml"
+                          attributeName="transform" type="translate"
+                          values="0 0; 0 20; 0 0"
+                          begin="0" dur="0.6s" repeatCount="indefinite"/>
+    </rect>
+                        <rect x="10" y="0" width="4" height="10" fill="#333">
+                            <animateTransform attributeType="xml"
+                                              attributeName="transform" type="translate"
+                                              values="0 0; 0 20; 0 0"
+                                              begin="0.2s" dur="0.6s" repeatCount="indefinite"/>
+                        </rect>
+                        <rect x="20" y="0" width="4" height="10" fill="#333">
+                            <animateTransform attributeType="xml"
+                                              attributeName="transform" type="translate"
+                                              values="0 0; 0 20; 0 0"
+                                              begin="0.4s" dur="0.6s" repeatCount="indefinite"/>
+                        </rect>
+  </svg>
+                </div>
+            </router-view>
         </div>
 
 
