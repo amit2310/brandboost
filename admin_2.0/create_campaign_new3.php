63ec62baab4d528804c49ddb689547676b4c29f3
amit2310 <amitadixtion@gmail.com>
2020-03-14 04:52:03 +0530

    bb 2.0

---
+++ admin_2.0/create_campaign_new3.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Brand Boost 2.0</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="icon" href="assets/images/icon.ico" sizes="16x16" type="image/ico">
+
+
+ <!--******************
+ CSS
+ **********************-->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="https://cdn.jsdelivr.net/npm/remixicon@2.3.0/fonts/remixicon.css" rel="stylesheet">
+<link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+<link href="assets/css/styleguide.css" rel="stylesheet" type="text/css">
+
+
+
+
+</head>
+<body id="ReviewSection">
+
+<div class="page-wrapper">
+ <!--******************
+ SIDEBAR
+ **********************-->
+  <?php include("sidebar.php"); ?>
+ 
+
+  <div class="page-content">
+ <!--******************
+  TOPBAR
+ **********************-->
+  <?php include("topbar.php"); ?>
+  
+  
+ <!--******************
+  Top Heading area
+ **********************-->
+  <div class="top-bar-top-section bbot">
+  <div class="container-fluid">
+   <div class="row">
+   	<div class="col-md-6">
+   	<span class="float-left mr20"><img src="assets/images/BACK.svg"/></span>
+   	<h3 class="htxt_medium_24 dark_700">Review Campaign</h3>
+   	</div>
+   	<div class="col-md-6 col-6 text-right">
+   		<button class="btn btn-md bkg_light_800 light_000" >Save Campaign <span><img src="assets/images/arrow-right-circle-fill-white.svg"></span></button>
+   	</div>
+   </div>
+   </div>
+    <div class="clearfix"></div>
+</div>
+	 
+	  
+	  
+	  
+ <!--******************
+  Content Area
+ **********************-->
+   <div class="content-area">
+    <div class="container-fluid">
+    
+    <div class="table_head_action pb0 mb15">
+    <div class="row">
+    	<div class="col">
+    		<h3 class="htxt_medium_14 dark_600">Email channel</h3>
+    	</div>
+    	<div class="col text-right">
+    	<p class="fsize14 dark_600 m-0">Active &nbsp; &nbsp;
+    	<label class="custom-form-switch mt-0">
+			<input class="field" type="checkbox" checked="">
+			<span class="toggle green3"></span>
+		</label>
+   		</p>
+    	</div>
+    </div>
+    </div>
+    
+    
+    <div class="row">
+    	<div class="col-12">
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_light_200 light_000 d-block dark_100 fsize16 fw500">1</span></div>
+    				<div class="col">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">From</h3>
+    					<p class="htxt_regular_14 dark_400 m-0 ls4">Who is sending this review request?</p>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 reviews_400 fsize13 fw500 p10 pl30 pr30 shadow-none">Add sender</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_light_200 light_000 d-block dark_100 fsize16 fw500">2</span></div>
+    				<div class="col">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Subject</h3>
+    					<p class="htxt_regular_14 dark_400 m-0 ls4">Add your email’s subject line</p>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 reviews_400 fsize13 fw500 p10 pl30 pr30 shadow-none">Add subject</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_light_200 light_000 d-block dark_100 fsize16 fw500">3</span></div>
+    				<div class="col">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Content</h3>
+    					<p class="htxt_regular_14 dark_400 m-0 ls4">Customize the content of your email.</p>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 reviews_400 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit content</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_light_200 light_000 d-block dark_100 fsize16 fw500">4</span></div>
+    				<div class="col">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Settings & Tracking</h3>
+    					<p class="htxt_regular_14 dark_400 m-0 ls4">Customize your default campaign’s settings</p>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 reviews_400 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit Settings</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		
+    	</div>
+    </div>
+    
+    
+    <div class="table_head_action pb0 mb15 mt15">
+    <div class="row">
+    	<div class="col">
+    		<h3 class="htxt_medium_14 dark_600">SMS channel</h3>
+    	</div>
+    	<div class="col text-right">
+    	<p class="fsize14 dark_600 m-0">Active &nbsp; &nbsp;
+    	<label class="custom-form-switch mt-0">
+			<input class="field" type="checkbox" checked >
+			<span class="toggle green3"></span>
+		</label>
+   		</p>
+    	</div>
+    </div>
+    </div>
+    
+    
+    
+    <div class="row">
+    	<div class="col-12">
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_light_200 light_000 d-block dark_100 fsize16 fw500">1</span></div>
+    				<div class="col">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">From</h3>
+    					<p class="htxt_regular_14 dark_400 m-0 ls4">Who is sending this review request?</p>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 reviews_400 fsize13 fw500 p10 pl30 pr30 shadow-none">Add sender</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_light_200 light_000 d-block dark_100 fsize16 fw500">2</span></div>
+    				<div class="col">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Content</h3>
+    					<p class="htxt_regular_14 dark_400 m-0 ls4">Customize the content of your email.</p>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 reviews_400 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit content</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_light_200 light_000 d-block dark_100 fsize16 fw500">3</span></div>
+    				<div class="col">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Settings & Tracking</h3>
+    					<p class="htxt_regular_14 dark_400 m-0 ls4">Customize your default campaign’s settings</p>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 reviews_400 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit Settings</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		
+    	</div>
+    </div>
+    
+  
+    
+    
+    
+     
+      </div>
+      
+      </div>
+      
+<!--******************
+  Content Area End
+ **********************-->
+  </div>
+  </div>
+  
+  
+ 
+ 
+
+ 
+ 
+ <!--******************
+  jQuery
+ **********************-->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/metisMenu.min.js"></script>
+<script src="assets/js/waves.min.js"></script>
+<script src="assets/js/jquery.slimscroll.min.js"></script>
+<script src="assets/js/app.js"></script>
+
+
+
+
+<script>
+	//side nav active script
+	$(".nav-link.review").addClass("active");
+	$(".nav-link.people").removeClass("active");
+	$(".main-icon-menu-pane.review").addClass("active");
+	$(".main-icon-menu-pane.people").removeClass("active");
+</script>
+
+</body>
+</html>
