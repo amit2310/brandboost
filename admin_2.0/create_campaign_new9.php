1a9185af81c92ea8647513edf94904da5f0dd9b7
amit2310 <amitadixtion@gmail.com>
2020-03-18 04:30:07 +0530

    bb 2.0

---
+++ admin_2.0/create_campaign_new9.php
@@ -0,0 +1,378 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Brand Boost 2.0</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="icon" href="assets/images/icon.ico" sizes="16x16" type="image/ico">
+
+
+ <!--******************
+ CSS
+ **********************-->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="https://cdn.jsdelivr.net/npm/remixicon@2.3.0/fonts/remixicon.css" rel="stylesheet">
+<link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+<link href="assets/css/styleguide.css" rel="stylesheet" type="text/css">
+
+
+
+
+</head>
+<body id="ReviewSection">
+
+<div class="page-wrapper">
+ <!--******************
+ SIDEBAR
+ **********************-->
+  <?php include("sidebar.php"); ?>
+ 
+
+  <div class="page-content">
+ <!--******************
+  TOPBAR
+ **********************-->
+  <?php include("topbar.php"); ?>
+  
+  
+ <!--******************
+  Top Heading area
+ **********************-->
+  <div class="top-bar-top-section bbot">
+  <div class="container-fluid">
+   <div class="row">
+   	<div class="col-md-6">
+   	<span class="float-left mr20"><img src="assets/images/BACK.svg"/></span>
+   	<h3 class="htxt_medium_24 dark_700">Review Campaign</h3>
+   	</div>
+   	<div class="col-md-6 col-6 text-right">
+   		<button class="btn btn-md bkg_reviews_400 light_000" >Save Campaign <span><img src="assets/images/arrow-right-circle-fill-white.svg"></span></button>
+   	</div>
+   </div>
+   </div>
+    <div class="clearfix"></div>
+</div>
+	 
+	  
+	  
+	  
+ <!--******************
+  Content Area
+ **********************-->
+   <div class="content-area">
+    <div class="container-fluid">
+    
+    
+    
+         <!--******************
+	 PAGE LEFT SIDEBAR
+	**********************-->
+	<a class="close_sidebar" href="#">&nbsp; <img src="assets/images/menu-2-line.svg"></a>
+	<div class="page_sidebar bkg_light_000 fixed">
+ 	<div style="width: 279px;">
+	 	<div class="p20 bbot top_headings">
+	 		<div class="row">
+	 			<div class="col"><p>Review Campaign</p></div>
+	 		</div>
+	 	</div>
+      
+      <div class="p30 pb10 text-center">
+      	<img src="assets/images/review_setup_graph5.svg"/>
+      </div>
+       
+      					<div class="p30 pb15">
+     						<div class="row">
+     							<div class="col-9">
+     								<p class="htxt_medium_12 dark_200 ls_4 m-0 ml20">EMAIL REQUESTS</p>
+     							</div>
+     							<div class="col-3">
+     								<label class="custom-form-switch mt-0 float-right">
+										<input class="field" type="checkbox" checked="">
+										<span class="toggle reviews"></span>
+									</label>
+     							</div>
+     						</div>
+      						<ul class="list_review mt-3">
+     							<li><a class="done" href="#">From</a></li>
+     							<li><a class="done" href="#">Email Subject</a></li>
+     							<li><a class="done" href="#">Content</a></li>
+     							<li><a class="done" href="#">Settings & Tracking</a></li>
+     						</ul>
+      					</div>
+      					
+      					<div class="pl30 pr30"><hr class="m-0"></div>
+      					
+      					<div class="p30 pb15">
+     						<div class="row">
+     							<div class="col-9">
+     								<p class="htxt_medium_12 dark_200 ls_4 m-0 ml20">SMS REQUESTS</p>
+     							</div>
+     							<div class="col-3">
+     								<label class="custom-form-switch mt-0 float-right">
+										<input class="field" type="checkbox" checked="">
+										<span class="toggle reviews"></span>
+									</label>
+     							</div>
+     						</div>
+      						<ul class="list_review mt-3">
+     							<li><a class="done" href="#">From</a></li>
+     							<li><a class="done" href="#">Content</a></li>
+     							<li><a class="done" href="#">Settings & Tracking</a></li>
+     						</ul>
+      					</div>
+      					
+      					
+      					
+      					<div class="reviw_howtouse">
+      						<a href="#" class="close_x_icon2"><img src="assets/images/close_grey_line.svg"/></a>
+      						<p class="fsize12 fw300 light_000">Learn how to use <br><strong>Campaigns</strong> in 60 seconds</p>
+							<img src="assets/images/review_play.svg"/>
+      					</div>
+        
+       
+
+    
+    <div class="clearfix"></div> 
+    </div>  
+</div>
+   <!--******************
+	 PAGE LEFT SIDEBAR END
+	**********************-->
+   
+   
+   
+   
+    <div class="table_head_action pb0 mb15">
+    <div class="row">
+    	<div class="col">
+    		<h3 class="htxt_medium_14 dark_600">Email channel</h3>
+    	</div>
+    	<div class="col text-right">
+    	<p class="fsize14 dark_600 m-0">Active &nbsp; &nbsp;
+    	<label class="custom-form-switch mt-0">
+			<input class="field" type="checkbox" checked="">
+			<span class="toggle green3"></span>
+		</label>
+   		</p>
+    	</div>
+    </div>
+    </div>
+    
+    
+    <div class="row">
+    	<div class="col-12">
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_reviews_000 reviews_400 d-block fsize16 fw500"><i class="ri-check-line"></i></span></div>
+    				<div class="col-9">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">From</h3>
+    					<ul class="review_camapaign_list">
+    						<li><span>From name</span><strong>Max Iver</strong></li>
+    						<li><span>From email address</span><strong>iver.mdx@brandboost.com</strong></li>
+    					</ul>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 dark_200 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_reviews_000 reviews_400 d-block fsize16 fw500"><i class="ri-check-line"></i></span></div>
+    				<div class="col-9">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Subject</h3>
+    					<ul class="review_camapaign_list">
+    						<li><span>Subject line</span><strong>Hello <strong class="reviews_400">[Fist name]</strong>, It was a pleasure doing business with you...</strong></li>
+    						<li><span>Email preview text</span><strong>Thank you for giving us a try 🙏</strong></li>
+    					</ul>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 dark_200 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_reviews_000 reviews_400 d-block fsize16 fw500"><i class="ri-check-line"></i></span></div>
+    				<div class="col-9">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Content</h3>
+    					<ul class="review_camapaign_list">
+    						<li><span><figure><img src="assets/images/Create_Ema_preview.png"/></figure></span><strong><a href="#">Preview Template “Review Request Email”</a> <br>
+    						<a href="#">Preview plain text version</a><br>
+							<a href="#">Send test email</a>
+    						</strong></li>
+    					</ul>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 dark_200 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_reviews_000 reviews_400 d-block fsize16 fw500"><i class="ri-check-line"></i></span></div>
+    				<div class="col-9">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Settings & Tracking</h3>
+    					<ul class="review_camapaign_list_check">
+    						<li><strong><i class="ri-checkbox-circle-fill"></i> Use conversation to manage replies</strong><strong><i class="ri-checkbox-circle-fill"></i> Google Analytics</strong></li>
+    						<li><strong><i class="ri-checkbox-circle-fill"></i> Track opens / read</strong><strong><i class="ri-checkbox-circle-fill"></i> Track clicks</strong></li>
+    					</ul>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 dark_200 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		
+    		
+    		
+    	</div>
+    </div>
+    
+    
+    <div class="table_head_action pb0 mb15 mt15">
+    <div class="row">
+    	<div class="col">
+    		<h3 class="htxt_medium_14 dark_600">SMS channel</h3>
+    	</div>
+    	<div class="col text-right">
+    	<p class="fsize14 dark_600 m-0">Active &nbsp; &nbsp;
+    	<label class="custom-form-switch mt-0">
+			<input class="field" type="checkbox" checked >
+			<span class="toggle green3"></span>
+		</label>
+   		</p>
+    	</div>
+    </div>
+    </div>
+    
+    
+    
+    <div class="row">
+    	<div class="col-12">
+    		
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_reviews_000 reviews_400 d-block fsize16 fw500"><i class="ri-check-line"></i></span></div>
+    				<div class="col-9">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">From</h3>
+    					<ul class="review_camapaign_list">
+    						<li><span>From name</span><strong>Makers</strong></li>
+    						<li><span>From phone number</span><strong>+1 (262) 555-0131</strong></li>
+    					</ul>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 dark_200 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_reviews_000 reviews_400 d-block fsize16 fw500"><i class="ri-check-line"></i></span></div>
+    				<div class="col-9">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Content</h3>
+    					<ul class="review_camapaign_list">
+    						<li>
+    						<span class="smsbox">
+    							<div class="row mb-1">
+    								<div class="col"><p class="m-0 fsize8 light_700"><img width="10" src="assets/images/message_icon_green.svg"/> &nbsp; MESSAGES</p></div>
+    								<div class="col text-right"><p class="m-0 fsize8 light_700">now</p></div>
+    							</div>
+    							<p class="fsize9 fw500 dark_900 mb-1">Makers</p>
+    							<p class="fsize9 fw300 dark_900 m-0">Hello Julia, It was a pleasure doing business with you. Thank you for giving us a try. You can click this link to leave your review...</p>
+    						</span>
+    						<strong>Hello <strong class="reviews_400">[Fist name]</strong>, It was a pleasure doing business with you... <br>
+    						<a href="#">Preview SMS Template “Review Request”</a><br>
+							<a href="#">Send test SMS</a>
+    						</strong></li>
+    					</ul>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 dark_200 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    		
+    		
+    		
+    		
+    		
+    		<div class="card p35 br6 mb10">
+    			<div class="row">
+    				<div style="max-width: 64px" class="col mt-1"><span class="circle-icon-36 bkg_reviews_000 reviews_400 d-block fsize16 fw500"><i class="ri-check-line"></i></span></div>
+    				<div class="col-9">
+    					<h3 class="htxt_medium_16 dark_700 mb-2">Settings & Tracking</h3>
+    					<ul class="review_camapaign_list_check">
+    						<li><strong><i class="ri-checkbox-circle-fill"></i> Use conversation to manage replies</strong><strong><i class="ri-checkbox-circle-fill"></i> Google Analytics</strong></li>
+    						<li><strong><i class="ri-checkbox-circle-fill"></i> Track opens / read</strong><strong><i class="ri-checkbox-circle-fill"></i> Track clicks</strong></li>
+    					</ul>
+    				</div>
+    				<div class="col text-right">
+    					<button class="btn border br35 dark_200 fsize13 fw500 p10 pl30 pr30 shadow-none">Edit</button>
+    				</div>
+    			</div>
+    		</div>
+    		
+    	</div>
+    </div>
+    
+  
+    
+    
+    
+     
+      </div>
+      
+      </div>
+      
+<!--******************
+  Content Area End
+ **********************-->
+  </div>
+  </div>
+  
+  
+ 
+ 
+
+ 
+ 
+ <!--******************
+  jQuery
+ **********************-->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/metisMenu.min.js"></script>
+<script src="assets/js/waves.min.js"></script>
+<script src="assets/js/jquery.slimscroll.min.js"></script>
+<script src="assets/js/app.js"></script>
+
+
+
+
+<script>
+	//side nav active script
+	$(".nav-link.review").addClass("active");
+	$(".nav-link.people").removeClass("active");
+	$(".main-icon-menu-pane.review").addClass("active");
+	$(".main-icon-menu-pane.people").removeClass("active");
+</script>
+<script>
+	$(document).ready(function(){
+	  $(".close_x_icon2").click(function(){
+		$(".reviw_howtouse").hide(200);
+	  });
+	});
+</script>
+</body>
+</html>
