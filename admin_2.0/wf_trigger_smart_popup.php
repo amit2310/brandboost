2291263cc3a9c1506f86cebf0b5d13e4d0a521ac
amit2310 <amitadixtion@gmail.com>
2020-04-16 23:27:07 +0530

    admin 2.0

---
+++ admin_2.0/wf_trigger_smart_popup.php
@@ -0,0 +1,48 @@
+<div class="box" style="width: 424px; display:block;">
+  <div style="width: 424px;overflow: hidden; height: 100%;">
+    <div style="height: 100%; overflow-y:auto; overflow-x: hidden;"> <a class="cross_icon slidebox"><i class=""><img src="assets/images/cross.svg"/></i></a>
+      <div class="p40">
+        <div class="row">
+          <div class="col-md-12"> <img width="44" src="assets/images/trigger_grey_45.png"/>
+            <h3 class="htxt_medium_24 dark_800 mt20">Edit Trigger </h3>
+            <p class="fsize14 dark_200 mb0 mt-1">Which event should trigger this automation?</p>
+            <hr class="mt25 mb30">
+          </div>
+          
+          
+           <div class="col-md-12">
+          <div class="form-group">
+                <label for="campaignname" class="fsize11 fw500 dark_600 ls4">SELECT TRIGGER</label>
+                <!--<input type="text" class="form-control h56" id="campaignname" placeholder="Enter campaign name" name="campaignname">-->
+                <select class="form-control h50 form-control-custom">
+                	<option>Choose a trigger...</option>
+                    <option>Submitted a form</option>
+                    <option>Choose a trigger...</option>
+                    <option>Choose a trigger...</option>
+                </select>
+              </div>
+          </div>
+          
+          
+        </div>
+
+
+        <div class="row bottom-position">
+          <div class="col-md-12 mb15">
+            <hr>
+          </div>
+          <div class="col-md-12">
+            <button class="btn btn-md bkg_blue_400 light_000 pr20 min_w_160 fsize13 fw500 mr20">Create Contact</button>
+            <button class="btn btn-md bkg_light_000 dark_200 pr20 fsize13 fw500 border">Close</button>
+            
+        </div>
+       
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
