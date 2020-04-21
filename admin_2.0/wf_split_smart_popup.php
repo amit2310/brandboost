a9b90f86bc3b3730ad01b945318d953de22c8d86
amit2310 <amitadixtion@gmail.com>
2020-04-22 04:54:24 +0530

    admin 2.0

---
+++ admin_2.0/wf_split_smart_popup.php
@@ -0,0 +1,102 @@
+<div class="box" style="width: 424px; display:block; border-color:#67B7E4!important">
+  <div style="width: 424px;overflow: hidden; height: 100%;">
+    <div style="height: 100%; overflow-y:auto; overflow-x: hidden;"> <a class="cross_icon slidebox"><i class=""><img src="assets/images/cross.svg"/></i></a>
+      <div class="p40">
+        <div class="row">
+          <div class="col-md-12"> <img width="44" src="assets/images/split_icon_44.svg"/>
+            <h3 class="htxt_medium_24 dark_800 mt20">Split Test</h3>
+            <p class="fsize14 dark_200 mb0 mt-1">Which event should trigger this automation?</p>
+            <hr class="mt25 mb30">
+          </div>
+          
+          
+           <div class="col-md-12">
+          	<div class="form-group">
+                <label for="campaignname" class="fsize11 fw500 dark_600 ls4">SPLIT TEST NAME</label>
+                <!--<select class="form-control h50 form-control-custom dark_800">
+                	<option>Submitted a form</option>
+                    <option>Submitted a form</option>
+                    <option>Choose a trigger...</option>
+                    <option>Choose a trigger...</option>
+                </select>-->
+                <input type="text" class="form-control h50" placeholder="Enter new split test name"/>
+                
+                
+                
+                
+                
+              </div>
+              
+              <div class="form-group mb30">
+                <label for="campaignname" class="fsize11 fw500 dark_600 ls4">SPLIT INTO</label>
+                <select class="form-control h50 form-control-custom dark_800">
+                	<option>2 paths (A/B)</option>
+                    <option>2 paths (A/B)</option>
+                    
+                </select>
+              </div>
+          </div>
+          
+          
+          <div class="col-md-9">
+              <div class="form-group">
+                <label for="campaignname" class="fsize11 fw500 dark_600 ls4">DISTRIBUTE TRAFFIC EVENLY</label>
+              </div>
+          </div>
+          
+          <div class="col-md-3">
+          <p class="m0 fsize11 email_400 fw500 float-left">YES</p>
+            <label class="custom-form-switch float-right">
+			  <input class="field" type="checkbox" checked="checked" >
+			<span class="toggle email"></span>		</label>
+          </div>
+          
+          
+          
+          
+          
+          
+          
+          
+          <div class="col-md-9">
+              <div class="form-group">
+                <label for="campaignname" class="fsize11 fw500 dark_600 ls4 text-uppercase">Conditional Split</label>
+              </div>
+          </div>
+          
+          <div class="col-md-3">
+          <p class="m0 fsize11 light_800 fw500 float-left">NO</p>
+            <label class="custom-form-switch float-right">
+			  <input class="field" type="checkbox" >
+			<span class="toggle email"></span>		</label>
+          </div>
+          
+          
+          
+          
+          
+          
+          
+          
+        </div>
+
+
+        <div class="row bottom-position">
+          <div class="col-md-12 mb15">
+            <hr>
+          </div>
+          <div class="col-md-12">
+            <button class="btn btn-md bkg_email_400 light_000 pr20 min_w_160 fsize13 fw500 mr20">Run Split Test</button>
+            <button class="btn btn-md bkg_light_000 dark_200 pr20 fsize13 fw500 border">Close</button>
+            
+        </div>
+       
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
