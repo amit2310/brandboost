e50ebe0ceb916c7368b5f970df685cc33dd1a4b6
andrew-bbio <andrew@brandboost.io>
2020-05-01 16:10:04 -0400

    Review Request Pages

--- resources/js/components/admin/brandboost/onsite/ReviewRequest.vue
+++ resources/js/components/admin/brandboost/onsite/ReviewRequest.vue
@@ -12,7 +12,7 @@
                     </div>
                     <div class="col-md-6 col-6 text-right">
                        <a :href="'admin/brandboost/export-review-request?sortBy='+sortBy+'&search='+searchBy"> <button class="circle-icon-40 mr15"><img src="assets/images/download-fill.svg"></button></a>
-                        <button class="btn btn-md bkg_reviews_400 light_000 ">SEND NEW REQUEST <span><img src="assets/images/reviews_plus_icon.svg"></span></button>
+                        <button class="btn btn-md bkg_reviews_400 light_000 ">Send New Request <span><img src="assets/images/reviews_plus_icon.svg"></span></button>
                     </div>
                 </div>
             </div>
@@ -128,69 +128,6 @@
                         </div>
                     </div>
                 </div>
-
-                <!--<div class="row" v-if="viewType == 'Grid View'">
-                    <div class="col-md-3 d-flex">
-                        <div class="card p0 pt30 text-center animate_top col">
-                            <span class="status_icon bkg_green_400"></span>
-                            <span class="check_icon green_400 fsize18"><i class="ri-check-double-line"></i></span>
-
-                            <div class="dot_dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false"> <img class="" src="assets/images/dots.svg" alt="profile-user"> </a>
-                                <div class="dropdown-menu dropdown-menu-right" x-placement="bottom-end" style="position: absolute; transform: translate3d(-136px, 18px, 0px); top: 0px; left: 0px; will-change: transform;"><a class="dropdown-item" href="#"><i class="dripicons-user text-muted mr-2"></i> Profile</a> <a class="dropdown-item" href="#"><i class="dripicons-wallet text-muted mr-2"></i> My Wallet</a> <a class="dropdown-item" href="#"><i class="dripicons-gear text-muted mr-2"></i> Settings</a> <a class="dropdown-item" href="#"><i class="dripicons-lock text-muted mr-2"></i> Lock screen</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Logout</a></div>
-                            </div>
-                            <a href="#" class="circle-icon-48 bkg_reviews_300 m0auto"><span class="fsize16 fw500 light_000 text-uppercase">CS</span> </a>
-                            <h3 class="htxt_medium_14 dark_600 mb-2 mt-3">Francisco Miles</h3>
-                            <p class="fsize14 dark_400 mb-1">francisco.m@example.com</p>
-                            <p class="fsize14 dark_400 mb20"><img src="assets/images/dot_blue_6.svg"/></i> New Customers</p>
-                            <div class="p20 btop">
-                                <div class="row">
-                                    <div class="col-7 text-left"><p class="fsize14 dark_400 m-0">11 min ago</p></div>
-                                    <div class="col-5 text-right"><p class="fsize14 dark_400 m-0"><i class="ri-star-fill sms_400"></i> 4.5</p></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3 d-flex" v-for="request in requests">
-                        <div class="card p0 pt30 text-center animate_top col">
-                            <div class="dot_dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="javascript:void(0);" role="button" aria-haspopup="false" aria-expanded="false"> <img class="" src="assets/images/dots.svg" alt="profile-user"> </a>
-                                <div class="dropdown-menu dropdown-menu-right" x-placement="bottom-end" style="position: absolute; transform: translate3d(-136px, 18px, 0px); top: 0px; left: 0px; will-change: transform;">
-                                    <a href="javascript:void(0);"><i class="dripicons-user text-muted mr-2"></i> Link 1</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="javascript:void(0);"><i class="dripicons-exit text-muted mr-2"></i> Link 2</a>
-                                </div>
-                            </div>
-                            <a href="javascript:void(0);" class="circle-icon-64 bkg_reviews_000 m0auto">
-                                <img v-if="request.subscriberstatus == 1" src="assets/images/review_campaign.png">
-                                <img v-else src="assets/images/review_campaign.png">
-                            </a>
-                            <span v-if="request.brand_title != ''">
-                                <h3 class="htxt_bold_16 dark_700 mb-2 mt-4">{{ setStringLimit(capitalizeFirstLetter(request.brand_title), 23) }}</h3>
-                            </span>
-                            <span v-else>No Data</span>
-                            <p>{{ setStringLimit(capitalizeFirstLetter(request.brand_desc), 31) }}</p>
-                            <p class="fsize10 fw500 light_800 text-uppercase mb20" v-if="request.subscriberstatus == 0" >INACTIVE</p>
-                            <p class="fsize10 fw500 green_400 text-uppercase mb20" v-if="request.subscriberstatus == 1" >ACTIVE</p>
-                            <p class="fsize10 fw500 light_800 text-uppercase mb20" v-if="request.subscriberstatus == 2" >PENDING</p>
-                            <p class="fsize10 fw500 light_800 text-uppercase mb20" v-if="request.subscriberstatus == 3" >ARCHIVED</p>
-                            <div class="p15 pt15 btop">
-                                <p class="htxt_regular_12 dark_300 mb15"><em> Sent On: {{ displayDateFormat('M d, Y', request.requestdate) }} </em></p>
-                                <p class="htxt_regular_12 dark_300">
-                                    <span class="table-img mr15">
-                                           <user-avatar
-                                               :avatar="''"
-                                               :firstname="request.firstname"
-                                               :lastname="request.lastname"
-                                           ></user-avatar>
-                                        </span>
-                                    <span class="htxt_medium_14 dark_900">{{ capitalizeFirstLetter(request.firstname) }} {{ capitalizeFirstLetter(request.lastname) }}</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>-->
 
                 <div class="row" v-if="viewType == 'Grid View'">
                     <div class="col-md-3 d-flex" v-for="request in requests">
